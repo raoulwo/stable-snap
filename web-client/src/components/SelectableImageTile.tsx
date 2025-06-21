d38eb872e4ad6f55f89fecee01402ce11621cf19
Daniel Puehringer <danielpuehringer11@gmail.com>
2025-06-21 18:51:23 +0200

    WIP click on image

--- web-client/src/components/SelectableImageTile.tsx
+++ web-client/src/components/SelectableImageTile.tsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import {Check} from "lucide-react";
+import {useState, useEffect} from "react";
+import {Check, Loader2} from "lucide-react";
 
 interface ImageProps {
     imageId: string;
@@ -10,17 +10,32 @@ const SelectableImageTile = ({ imageId, imageURL = "/construction_progress-middl
     const [imageUrl] = useState<string>(imageURL);
     const [loading] = useState(false);
     const [selected, setSelected] = useState(false);
+    const localStorageKey = `Stable-Snap-${imageId}`;
+
+    // Checks localStorage for a previously saved selection state for this specific image and restores selection state
+    useEffect(() => {
+        const stored = localStorage.getItem(localStorageKey);
+        setSelected(stored === "true");
+    }, []);
 
     const handleSelectionToggle = (event:any):void => {
         event.stopPropagation();
         setSelected(!selected);
+        //TODO
         console.log("selected clicked");
+    }
+
+    const handleFocusedImage = (): void => {
+        console.log("when this is clicked, the focused image will be set");
+        console.log("image selected: "+imageURL+" with id:" + imageId);//TODO
     }
 
     return (
         <div id="SelectableImageTile" className="rounded-md">
             {loading ? (
-                <div>Loading</div>
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                    <Loader2 size={48} className="animate-spin mt-6" />
+                </div>
             ):(
                 <>
                     <div
@@ -33,7 +48,9 @@ const SelectableImageTile = ({ imageId, imageURL = "/construction_progress-middl
                     <img
                         className="rounded shadow-md border-1 border-gray-400"
                         src={imageUrl}
-                        alt={`Image with id: ${imageId}`} />
+                        alt={`Image with id: ${imageId}`}
+                        onClick={handleFocusedImage}
+                    />
                 </>
             )}
         </div>
