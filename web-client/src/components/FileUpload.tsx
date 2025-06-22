87656f7a9b1981bc7e7b24a24920162cec1b25b8
Raoul Wograndl <90349386+RaoulWo@users.noreply.github.com>
2025-06-22 11:22:00 +0200

    feat: implement image upload

--- web-client/src/components/FileUpload.tsx
+++ web-client/src/components/FileUpload.tsx
@@ -1,4 +1,42 @@
-//https://tailwindcss.com/plus/ui-blocks/application-ui/forms/form-layouts
-/*
-Idea: create button and upload field for image. when button is clicked, file is uploaded
- */
+import React, { type ChangeEvent, useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button.tsx";
+import { uploadImage } from "@/lib/api.ts";
+
+const FileUpload: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length <= 0) {
+      setSelectedFile(null);
+      return;
+    }
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleImageUpload = async () => {
+    if (!selectedFile) {
+      return;
+    }
+
+    try {
+      await uploadImage(selectedFile);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="grid w-full max-w-sm items-center gap-3">
+        <Label htmlFor="image">Upload Image</Label>
+        <Input id="image" type="file" onChange={handleFileChange} />
+      </div>
+      <Button onClick={handleImageUpload}>Upload Image</Button>
+    </div>
+  );
+};
+
+export default FileUpload;
--- web-client/src/components/ProjectPage.tsx
+++ web-client/src/components/ProjectPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import SelectedImageDisplay from "@/components/SelectedImageDisplay.tsx";
 import SelectionPanel from "@/components/SelectionPanel.tsx";
+import FileUpload from "@/components/FileUpload.tsx";
 // import { Button } from "@/components/ui/button";
 
 const ProjectPage: React.FC = () => {
@@ -9,14 +10,19 @@ const ProjectPage: React.FC = () => {
     }, []);
 
     return (
+      <div>
         <div className="flex flex-col md:flex-row" id="ProjectPage">
-            <div className="w-full md:w-1/2 lg:w-2/5 p-2 mr-4 lg:mr-6 xl:mr-8">
-                <SelectedImageDisplay />
-            </div>
-            <div className="w-full md:w-1/2 lg:w-3/5 p-2">
-                <SelectionPanel/>
-            </div>
+          <div className="w-full md:w-1/2 lg:w-2/5 p-2 mr-4 lg:mr-6 xl:mr-8">
+            <SelectedImageDisplay />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-3/5 p-2">
+            <SelectionPanel/>
+          </div>
         </div>
+        <div className="mt-8">
+          <FileUpload />
+        </div>
+      </div>
     );
 }
 
