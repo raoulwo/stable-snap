5aeb9236d34da4f33727e3824f9495d334d0021b
pghgh <68028868+pghgh@users.noreply.github.com>
2025-06-22 12:04:51 +0200

    added initial setup for Amazon Amplify Authenticator, powered by Amazon Cognito

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,10 +1,28 @@
+/*
+TAKEN FROM 1
+
+The lines of code for setting up Amplify Authenticator powered by Amazon Cognito were taken from the following source:
+https://docs.amplify.aws/react/start/quickstart/
+*/
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// TAKEN FROM START 1
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 import App from './App.tsx'
+// TAKEN FROM END 1
+import './index.css'
 
+// TAKEN FROM START 1
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure(outputs);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Authenticator>
+          <App />
+      </Authenticator>
   </StrictMode>,
 )
+// TAKEN FROM END 1
