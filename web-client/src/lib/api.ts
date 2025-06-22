eb6ca02890229ebe0a89fd6021cd7e7d4ac251a8
Raoul Wograndl <90349386+RaoulWo@users.noreply.github.com>
2025-06-22 22:12:52 +0200

    feat: implement delete image api call

--- web-client/src/lib/api.ts
+++ web-client/src/lib/api.ts
@@ -2,6 +2,7 @@ import type { SearchResponse, SearchResult, UploadUrlResult } from './types';
 
 const SEARCH_API_INVOKE_URL = 'https://a7bcbzq751.execute-api.eu-central-1.amazonaws.com';
 const UPLOAD_API_INVOKE_URL = 'https://3v58kycltg.execute-api.eu-central-1.amazonaws.com';
+const DELETE_API_INVOKE_URL = 'https://3pr2btm7f9.execute-api.eu-central-1.amazonaws.com';
 
 export async function searchInitialImages(): Promise<{
   imageResults: SearchResult[];
@@ -40,6 +41,13 @@ export async function uploadImage(file: File): Promise<void> {
       'Content-Type': file.type,
     },
     body: file
+  });
+}
+
+export async function deleteImage(id: string): Promise<void> {
+  const url = `${DELETE_API_INVOKE_URL}/image/${id}`;
+  await fetch(url, {
+    method: 'DELETE',
   });
 }
 
